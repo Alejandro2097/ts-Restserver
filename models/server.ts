2ea3fce8698f4e1676d8edf1bc4c00f8557e8c63
Alejandro2097 <alejo12381@hotmail.es>
2022-01-12 23:04:41 -0500

    inicios de las rutas de la app

--- models/server.ts
+++ models/server.ts
@@ -1,11 +1,20 @@
 import express, { Application }from 'express';
+import userRoutes from '../routes/usuario';
 
 class Server{
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
     constructor(){
         this.app  = express();
         this.port = process.env.PORT || '8000';
+        //Definir las rutas
+        this.routes();
+    }
+    routes(){
+        this.app.use(this.apiPaths.usuarios , userRoutes);
     }
     listen(){
         this.app.listen(this.port, () =>{ 
